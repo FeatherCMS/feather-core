ddb0e2b72ce76317dc41ac02f3b303420d9167a1
Tibor Bödecs <mail.tib@gmail.com>
2020-08-29 13:44:44 +0200

    elp + erase

---
+++ Sources/FeatherCore/EventLoopFuture+Erase.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tibor Bodecs on 2020. 08. 29..
+//
+
+public extension EventLoopFuture {
+
+    func erase() -> EventLoopFuture<Any> {
+        self.map { $0 as Any }
+    }
+
+    func erase() -> EventLoopFuture<Any?> {
+        self.map { $0 as Any? }
+    }
+}
