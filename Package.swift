8d6859f3e3496df27085a46c7594b77a744cd28a
Tibor Bödecs <mail.tib@gmail.com>
2020-12-01 13:46:28 +0100

    Updated deps (Liquid)

--- Package.swift
+++ Package.swift
@@ -10,14 +10,14 @@ let package = Package(
         .library(name: "FeatherCore", targets: ["FeatherCore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor", from: "4.35.0"),
+        .package(url: "https://github.com/vapor/vapor", from: "4.36.0"),
         .package(url: "https://github.com/vapor/leaf", .exact("4.0.0-tau.1")),
         .package(url: "https://github.com/vapor/leaf-kit", .exact("1.0.0-tau.1.1")),
         .package(url: "https://github.com/vapor/fluent", from: "4.0.0"),
         .package(url: "https://github.com/vapor/jwt", from: "4.0.0"),
         .package(url: "https://github.com/binarybirds/content-api", from: "1.0.0"),
         .package(url: "https://github.com/binarybirds/view-kit", from: "1.3.0-beta"),
-        .package(url: "https://github.com/binarybirds/liquid", from: "1.0.0"),
+        .package(url: "https://github.com/binarybirds/liquid", from: "1.2.0-beta"),
         .package(url: "https://github.com/binarybirds/viper-kit", from: "1.5.0-beta"),
     ],
     targets: [
