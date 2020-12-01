f78a13a98d06e1a7355d112876dd4f14561662d0
Tibor Bödecs <mail.tib@gmail.com>
2020-12-01 14:15:01 +0100

    revert Vapor version

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "FeatherCore", targets: ["FeatherCore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor", from: "4.36.0"),
+        .package(url: "https://github.com/vapor/vapor", from: "4.34.0"),
         .package(url: "https://github.com/vapor/leaf", .exact("4.0.0-tau.1")),
         .package(url: "https://github.com/vapor/leaf-kit", .exact("1.0.0-tau.1.1")),
         .package(url: "https://github.com/vapor/fluent", from: "4.0.0"),
