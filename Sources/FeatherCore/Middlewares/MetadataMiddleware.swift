02042a3eeaa6499de6b2b4f3ac6f97afb80c93fe
Tibor Bödecs <mail.tib@gmail.com>
2020-12-17 17:29:03 +0100

    metadata middleware fixes

--- Sources/FeatherCore/Middlewares/MetadataMiddleware.swift
+++ Sources/FeatherCore/Middlewares/MetadataMiddleware.swift
@@ -18,22 +18,33 @@ public struct MetadataModelMiddleware<T: MetadataRepresentable>: ModelMiddleware
 
     /// on create action we call the willUpdate method using the delegate
     public func create(model: T, on db: Database, next: AnyModelResponder) -> EventLoopFuture<Void> {
-        next.create(model, on: db) .flatMap {
-            Feather.metadataDelegate?.create(model.metadata, on: db) ?? db.eventLoop.future()
+        next.create(model, on: db).flatMap {
+            var metadata = model.metadata
+            metadata.id = UUID()
+            metadata.module = T.Module.name
+            metadata.model = T.name
+            metadata.reference = model.id
+            return Feather.metadataDelegate?.create(model.metadata, on: db) ?? db.eventLoop.future()
         }
     }
     
     /// on update action we call the willUpdate method using the delegate
     public func update(model: T, on db: Database, next: AnyModelResponder) -> EventLoopFuture<Void> {
         next.update(model, on: db).flatMap {
-            Feather.metadataDelegate?.update(model.metadata, on: db) ?? db.eventLoop.future()
+            var metadata = model.metadata
+            metadata.slug = nil
+            Feather.metadataDelegate?.update(metadata, on: db) ?? db.eventLoop.future()
         }
     }
     
     /// on delete action, we remove the associated metadata
     public func delete(model: T, force: Bool, on db: Database, next: AnyModelResponder) -> EventLoopFuture<Void> {
         next.delete(model, force: force, on: db).flatMap {
-            Feather.metadataDelegate?.delete(model.metadata, on: db) ?? db.eventLoop.future()
+            var metadata = model.metadata
+            metadata.module = T.Module.name
+            metadata.model = T.name
+            metadata.reference = model.id
+            Feather.metadataDelegate?.delete(metadata, on: db) ?? db.eventLoop.future()
         }
     }
 }
