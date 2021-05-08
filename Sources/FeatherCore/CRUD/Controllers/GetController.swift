0df86f4f33a85cab8f4d84a7d7125af39da3ebe4
MARTIN Denis <denis@irlmobile.com>
2021-05-08 23:07:46 +0200

    Forgot to remove the Auth in Public API Get

--- Sources/FeatherCore/CRUD/Controllers/GetController.swift
+++ Sources/FeatherCore/CRUD/Controllers/GetController.swift
@@ -116,14 +116,9 @@ public extension GetController {
     }
     
     func getPublicApi(_ req: Request) throws -> EventLoopFuture<GetApi.GetObject> {
-        accessGet(req: req).throwingFlatMap { hasAccess in
-            guard hasAccess else {
-                return req.eventLoop.future(error: Abort(.forbidden))
-            }
-            let id = try identifier(req)
-            return findBy(id, on: req.db).map { model in
-                GetApi().mapGet(model: model as! GetApi.Model)
-            }
+        let id = try identifier(req)
+        return findBy(id, on: req.db).map { model in
+            GetApi().mapGet(model: model as! GetApi.Model)
         }
     }
     
