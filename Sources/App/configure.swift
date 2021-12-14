b0e0d4d4e34720b14915b2bdec995007ed730550
Tibor Bödecs <mail.tib@gmail.com>
2021-12-14 14:39:31 +0100

    Added liquid driver & blog basics

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,13 +4,18 @@ import FluentSQLiteDriver
 import SwiftHtml
 import SwiftCss
 import FeatherCore
-
+import Liquid
+import LiquidLocalDriver
 
 public func configure(_ app: Application) throws {
 
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.databases.use(.sqlite(.file("Resources/db.sqlite")), as: .sqlite)
 
+    app.fileStorages.use(.local(publicUrl: Application.baseUrl,
+                                publicPath: Application.Paths.public.path,
+                                workDirectory: Application.Directories.assets), as: .local)
+    
     try Feather().start(app)
 
 //    app.routes.get("style.css") { req in
