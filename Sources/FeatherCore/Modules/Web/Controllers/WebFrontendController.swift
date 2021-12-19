14c631f3a3ee42902cb0471ded9f0bf9c8c5f355
Tibor Bödecs <mail.tib@gmail.com>
2021-12-19 15:13:00 +0100

    robots

--- Sources/FeatherCore/Modules/Web/Controllers/WebFrontendController.swift
+++ Sources/FeatherCore/Modules/Web/Controllers/WebFrontendController.swift
@@ -22,18 +22,26 @@ struct WebFrontendController {
                                                               message: "Lorem ipsum dolor sit amet"))
         return req.html.render(template)
     }
-    
+
     func renderRssTemplate(_ req: Request) async throws -> Response {
         let template = WebWelcomePageTemplate(req, context: .init(index: .init(title: "title"),
                                                               title: "Hello, World!",
                                                               message: "Lorem ipsum dolor sit amet"))
         return req.html.render(template)
     }
-    
+
     func renderRobotsTemplate(_ req: Request) async throws -> Response {
-        let template = WebWelcomePageTemplate(req, context: .init(index: .init(title: "title"),
-                                                              title: "Hello, World!",
-                                                              message: "Lorem ipsum dolor sit amet"))
-        return req.html.render(template)
+        let robots = """
+            Sitemap: #(baseUrl)\(Feather.config.paths.sitemap.safePath())
+
+            User-agent: *
+            Disallow: \(Feather.config.paths.admin.safePath())
+            Disallow: \(Feather.config.paths.api.safePath())
+            """
+        return Response(status: .ok,
+                        headers: [
+                            "content-type": "text/plain"
+                        ],
+                        body: .init(string: robots))
     }
 }
