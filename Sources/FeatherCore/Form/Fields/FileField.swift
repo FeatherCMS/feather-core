4ce925b2616c4f0af3dc28507fc02479f47887da
Tibor Bodecs <mail.tib@gmail.com>
2022-01-14 19:47:31 +0100

    file field fixes

--- Sources/FeatherCore/Form/Fields/FileField.swift
+++ Sources/FeatherCore/Form/Fields/FileField.swift
@@ -5,4 +5,15 @@
 //  Created by Tibor Bodecs on 2021. 12. 01..
 //
 
+public final class FileField: AbstractFormField<File, FileFieldTemplate> {
+
+    public convenience init(_ key: String) {
+        self.init(key: key, input: .init(data: "", filename: ""), output: .init(.init(key: key)))
+    }
+
+    public override func render(req: Request) -> TemplateRepresentable {
+        output.context.error = error
+        return super.render(req: req)
+    }
+}
 
