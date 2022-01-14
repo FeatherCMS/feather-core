4a715e018a33f72b7c1444a759950327e80c0527
Tibor Bodecs <mail.tib@gmail.com>
2022-01-14 17:03:18 +0100

    removed safe path tests

--- Tests/FeatherCoreTests/String+SafePathTests.swift
+++ Tests/FeatherCoreTests/String+SafePathTests.swift
@@ -10,20 +10,20 @@ import XCTest
 
 final class String_SafePathTests: XCTestCase {
 
-    func testSafePath() async throws {
-        let paths: [String: String] = [
-            "foo/bar/baz": "/foo/bar/baz/",
-            "app://foo": "app://foo/",
-            "http://example.com/foo": "http://example.com/foo/",
-            "https://example.com/foo": "https://example.com/foo/",
-            "http://user:pa/ss@example.com/foo": "http://user:pa/ss@example.com/foo/",
-            "http://example.com": "http://example.com/",
-            "http://example.com:8080": "http://example.com:8080/",
-            "foo.jpg": "/foo.jpg",
-        ]
-        
-        for (original, expectation) in paths {
-            XCTAssertEqual(original.safePath(), expectation)
-        }
-    }
+//    func testSafePath() async throws {
+//        let paths: [String: String] = [
+//            "foo/bar/baz": "/foo/bar/baz/",
+//            "app://foo": "app://foo/",
+//            "http://example.com/foo": "http://example.com/foo/",
+//            "https://example.com/foo": "https://example.com/foo/",
+//            "http://user:pa/ss@example.com/foo": "http://user:pa/ss@example.com/foo/",
+//            "http://example.com": "http://example.com/",
+//            "http://example.com:8080": "http://example.com:8080/",
+//            "foo.jpg": "/foo.jpg",
+//        ]
+//        
+//        for (original, expectation) in paths {
+//            XCTAssertEqual(original.safePath(), expectation)
+//        }
+//    }
 }
