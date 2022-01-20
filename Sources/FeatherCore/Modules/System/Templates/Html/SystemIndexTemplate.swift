ea06d4973f8b29bb81c156e016bcf46c00c629a1
Tibor Bodecs <mail.tib@gmail.com>
2022-01-20 18:43:16 +0100

    install template dark logo

--- Sources/FeatherCore/Modules/System/Templates/Html/SystemIndexTemplate.swift
+++ Sources/FeatherCore/Modules/System/Templates/Html/SystemIndexTemplate.swift
@@ -46,9 +46,14 @@ public struct SystemIndexTemplate: TemplateRepresentable {
                 Header {
                     Div {
                         A {
-                            Img(src: "/img/web/logo.png", alt: "Logo of Feather CMS")
-                                .title("Feather CMS")
-                                .style("width: 300px")
+                            Picture {
+                                Source()
+                                    .srcset("/img/web/logo-dark.png")
+                                    .media(value: "(prefers-color-scheme: dark)")
+                                Img(src: "/img/web/logo.png", alt: "Logo of Feather CMS")
+                                    .title("Feather CMS")
+                                    .style("width: 300px")
+                            }
                         }
                         .href("/")
                     }
