cf45c20f24d7ba3b87a190a397d106025f5e63ab
Tibor Bodecs <mail.tib@gmail.com>
2022-02-02 22:11:01 +0100

    fix login issue & css

--- Sources/FeatherCore/Modules/User/Authenticators/UserAccountCredentialsAuthenticator.swift
+++ Sources/FeatherCore/Modules/User/Authenticators/UserAccountCredentialsAuthenticator.swift
@@ -12,6 +12,20 @@ struct UserAccountCredentialsAuthenticator: AsyncCredentialsAuthenticator {
         let password: String
     }
     
+    // @NOTE: https://github.com/vapor/vapor/issues/2735
+    func authenticate(request: Request) async throws {
+        _ = try await request.body.collect(max: nil).get()
+
+        let credentials: Credentials
+        do {
+            credentials = try request.content.decode(Credentials.self)
+        }
+        catch {
+            return
+        }
+        return try await self.authenticate(credentials: credentials, for: request)
+    }
+    
     func authenticate(credentials: Input, for req: Request) async throws {
         guard let model = try await UserAccountModel.findWithPermissionsBy(email: credentials.email, on: req.db) else {
             return
@@ -19,7 +33,5 @@ struct UserAccountCredentialsAuthenticator: AsyncCredentialsAuthenticator {
         if let isValid = try? Bcrypt.verify(credentials.password, created: model.password), isValid {
             req.auth.login(model.featherAccount)
         }
-        
     }
 }
-
