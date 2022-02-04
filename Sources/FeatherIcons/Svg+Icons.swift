619d9cb3069fc7d665e21c899f166c8c6d993a58
Tibor Bodecs <mail.tib@gmail.com>
2022-02-04 17:31:42 +0100

    feather icons [wip]

---
+++ Sources/FeatherIcons/Svg+Icons.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tibor Bodecs on 2022. 02. 04..
+//
+
+import SwiftSvg
+import SwiftSgml
+
+public extension Svg {
+
+    static func icon(_ value: String) -> Svg? {
+        guard let icon = Icons(rawValue: value) else {
+            return nil
+        }
+        return .icon(icon)
+    }
+    
+    static func icon(_ value: Icons) -> Svg {
+        Svg {
+            value.tags
+        }
+        .width(24)
+        .height(24)
+        .viewBox(minX: 0, minY: 0, width: 24, height: 24)
+        .fill("none")
+        .stroke("currentColor")
+        .strokeWidth(2)
+        .strokeLinecap("round")
+        .strokeLinejoin("round")
+    }
+}
+
+
